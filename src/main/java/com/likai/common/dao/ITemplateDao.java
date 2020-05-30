6b9507e6bfe34d41e5656b7c6bb20777383f684f
likai <1414551714@qq.com>
2020-05-30 20:17:22 +0800

    补充通用式插入一条或多条数据方法，但使用时需注意手动配置mybatis扫描路径

---
+++ src/main/java/com/likai/common/dao/ITemplateDao.java
@@ -0,0 +1,38 @@
+package com.likai.common.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@Mapper
+public interface ITemplateDao {
+
+	/**
+	 * 通用型批量插入（不限定表，不限定字段数量，不支持自增主键的反向填充）<BR/>
+	 * 参数说明:表名必须与数据库一致，大小写不限。数据中一个map表示一行数据库记录，且map键必须与数据库一致，大小写不限，两个map之间put顺序必须一致
+	 * 
+	 * @param tableName
+	 *            表名
+	 * @param paramMapList
+	 *            数据
+	 * @return 影响行数
+	 * @author likai 2019年7月21日 下午10:40:24
+	 */
+	public int batchInsert(@Param("tableName") String tableName,
+                           @Param("paramMapList") List<LinkedHashMap<String, Object>> paramMapList);
+
+	/**
+	 * 通用型单行插入
+	 *
+	 * @param tableName
+	 *            表名
+	 * @param paramMap
+	 *            数据
+	 * @return 影响行数
+	 * @author likai 2019年7月23日 下午8:18:43
+	 */
+	public int oneInsert(@Param("tableName") String tableName,
+                         @Param("paramMap") LinkedHashMap<String, Object> paramMap);
+}
